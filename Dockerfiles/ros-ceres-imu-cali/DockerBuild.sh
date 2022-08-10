abd3b7bf13304ee5a9ba4cde3487a8d6ca30924b
hih313 <31838134+hihi313@users.noreply.github.com>
2022-08-10 01:40:02 +0000

    add calibration image & move submodule

---
+++ Dockerfiles/ros-ceres-imu-cali/DockerBuild.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+echo "Sart time=$(date +"%T")"
+IMG_NAME="hihi313/ros-ceres-imu-cali"
+IMG_TAG="latest"
+CTNR_NAME="ros_ctnr"
+CTNR_BASE_DIR="/app"
+
+while getopts "i:t:b:r:e" opt
+do
+  case $opt in
+    i)
+        IMG_NAME="$OPTARG"
+        ;;
+    t)
+        IMG_TAG="$OPTARG"
+        ;;
+    b) 
+        if [ "$OPTARG" == "n" ]
+        then
+            CACHE="--no-cache"
+        else
+            CACHE=""
+        fi
+        START="$(TZ=UTC0 printf '%(%s)T\n' '-1')" # `-1`  is the current time
+        
+        docker rmi $IMG_NAME
+        docker build $CACHE -t $IMG_NAME .
+        
+        # Pring elapsed time
+        ELAPSED=$(( $(TZ=UTC0 printf '%(%s)T\n' '-1') - START ))
+        TZ=UTC0 printf 'Build duration=%(%H:%M:%S)T\n' "$ELAPSED"
+        ;;
+    r)
+        if [ "$OPTARG" == "m" ]
+        then
+            RM="--rm"
+        else
+            RM=""
+        fi
+        # Enable tracing
+        set -x
+        # --mount type=volume,src="",dst="" \
+        # --mount type=bind,src="",dst="" \
+        # --user="$(id -u):$(id -g)" \
+        sudo xhost +local:root
+        docker run $RM -it --init \
+            --ipc=host \
+            -e "DISPLAY" \
+            --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+            --volume="$PWD:/app" \
+            --name $CTNR_NAME \
+            "$IMG_NAME:$IMG_TAG"
+        # Disable tracing
+        set +x        
+        ;;
+    e)
+        # Enable tracing
+        set -x
+        docker exec -it $CTNR_NAME bash
+        # Disable tracing
+        set +x        
+        ;;
+    \?) 
+        echo "Invalid option -$OPTARG" >&2
+        exit 1
+        ;;
+    :)
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+    *)
+        echo "*"
+        ;;
+  esac
+done
