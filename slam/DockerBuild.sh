05337baf873e48eb99543946c419f62776a5b908
hih313 <31838134+hihi313@users.noreply.github.com>
2022-10-05 16:44:32 +0800

    fixed g2o API syntex error

--- slam/DockerBuild.sh
+++ slam/DockerBuild.sh
@@ -7,34 +7,35 @@ IMG_TAG="latest"
 CTNR_NAME="slam_ctnr"
 WORKDIR="/app" # Should be the same as Dockerfile
 
-while getopts "i:t:b:r:e" opt
-do
-  case $opt in
+GPU=""
+while getopts "i:t:b:gr:e" opt; do
+    case $opt in
     i)
         IMG_NAME="$OPTARG"
         ;;
     t)
         IMG_TAG="$OPTARG"
         ;;
-    b) 
-        if [ "$OPTARG" == "n" ]
-        then
+    b)
+        if [ "$OPTARG" == "n" ]; then
             CACHE="--no-cache"
         else
             CACHE=""
         fi
         START="$(TZ=UTC0 printf '%(%s)T\n' '-1')" # `-1`  is the current time
-        
+
         docker rmi $IMG_NAME
         docker build $CACHE -t $IMG_NAME:$IMG_TAG
-        
+
         # Pring elapsed time
-        ELAPSED=$(( $(TZ=UTC0 printf '%(%s)T\n' '-1') - START ))
+        ELAPSED=$(($(TZ=UTC0 printf '%(%s)T\n' '-1') - START))
         TZ=UTC0 printf 'Build duration=%(%H:%M:%S)T\n' "$ELAPSED"
         ;;
+    g)
+        GPU="--gpus all"
+        ;;
     r)
-        if [ "$OPTARG" == "m" ]
-        then
+        if [ "$OPTARG" == "m" ]; then
             RM="--rm"
         else
             RM=""
@@ -44,15 +45,16 @@ do
         # --mount type=volume,src="",dst="" \
         # --mount type=bind,src="",dst="" \
         # --user="$(id -u):$(id -g)" \
+
         sudo xhost +local:root
         docker run --privileged \
             $RM \
+            $GPU \
             -it \
             --ipc=host \
             -p 8087:8087 \
             -e DISPLAY="host.docker.internal:0" \
             -e QT_X11_NO_MITSHM=1 \
-            --gpus all \
             -v /dev:/dev:ro \
             --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
             --mount type=volume,src="vscode-extensions",dst="/root/.vscode-server/extensions" \
@@ -62,16 +64,16 @@ do
             "$IMG_NAME:$IMG_TAG"
 
         # Disable tracing
-        set +x        
+        set +x
         ;;
     e)
         # Enable tracing
         set -x
         docker exec -it $CTNR_NAME bash
         # Disable tracing
-        set +x        
+        set +x
         ;;
-    \?) 
+    \?)
         echo "Invalid option -$OPTARG" >&2
         exit 1
         ;;
@@ -82,5 +84,5 @@ do
     *)
         echo "*"
         ;;
-  esac
+    esac
 done
